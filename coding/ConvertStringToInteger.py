5a64620da19e9e9efd5dfaedd3b7f5ae281fd6f6
parveendogra2@gmail.com <Parveendogra2@gmail.com>
2025-12-09 13:33:53 +0530

    Coding Question of Python_04

--- coding/ConvertStringToInteger.py
+++ coding/ConvertStringToInteger.py
@@ -1,5 +1,20 @@
 # Convert String To Inetger
+from unicodedata import decimal
 
-value_text = "Parveen123456Chaudhary"
-value_inter = int(input(value_text))
-print(value_inter)
+value_text = input(" Enter the value : ")
+if value_text.isdigit():
+    value_inter = int(value_text)
+    print("Entered value is number", value_inter)
+else:
+    print("Entered value is character", value_text)
+
+print(type(value_inter))
+
+# Handle through Try and catch
+
+value_text1 = input("Enter any value : ")
+try:
+    value_inter1 = int(value_text1)
+    print("Converted value is :", value_inter1)
+except ValueError:
+    print("")
---
+++ coding/PrimeNumber.py
@@ -0,0 +1,6 @@
+# Check the Prime Number
+
+# Prime numbers must be greater than 1
+#  if n <= 1:
+#         return False
+# If n is 1, 0, or negative, the function immediately returns False, meaning “not prime”.
