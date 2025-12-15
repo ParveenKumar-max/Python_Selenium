54e1ca1bc822f9d20d576a4e9f43461677329368
parveendogra2@gmail.com <Parveendogra2@gmail.com>
2025-12-15 18:40:51 +0530

    Coding question of Python Intermediate_04

---
+++ coding/Intermediate_Level/TwoSumProblems.py
@@ -0,0 +1,10 @@
+# Two sum problems
+
+# The Two Sum problem asks, you to find two numbers in an array whose sum equals a given target,
+# and return their indices.
+
+
+# for i, num in enumerate(nums):
+# Loops through the list:
+#
+# i → index , num → value at that index
