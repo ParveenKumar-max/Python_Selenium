7e642fc5ee9df6cfcb439e4a1277bfcbf53a32a0
parveendogra2@gmail.com <Parveendogra2@gmail.com>
2025-12-19 10:24:57 +0530

    Coding question of Python Intermediate _11

---
+++ coding/Intermediate_Level/ListComprehensive.py
@@ -0,0 +1,22 @@
+# List Comprehensive --> It is a concise way to create lists using an
+# expression, loop, and optional condition in a single line.”
+
+val_list = [2,3,4,5,8]
+val_rest = [i ** 2 for i in val_list]
+
+print(f"Value of List {val_list} Comprehensive is : {val_rest}")
+
+# For loop vs List Comprehensive
+
+val_list1 = [3,4,5,6,7]
+var_for = []
+for num in val_list1:
+    inum = num * 2
+    var_for.append(inum)
+print(f"value of for loop are : {var_for}")
+
+# Conditional Statement
+
+var_list2 = [1,3,5,7,9, 12]
+var_condition = [num1 for num1 in var_list2 if num1 % 2 == 0]
+print(f"Conditional List Comprehensive values are: {var_condition}")
