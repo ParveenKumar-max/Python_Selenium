23aaf9312c7f6c3699cfdfe191972f06fece44c7
parveendogra2@gmail.com <Parveendogra2@gmail.com>
2025-12-29 22:41:32 +0530

    Coding question of Python Intermediate_16

---
+++ coding/Begineer_Level/TestCode.py
@@ -0,0 +1,22 @@
+list = [123, 123, 'abc', 'abc']
+list.append(456)
+list.remove('abc')
+print(list)
+
+tuple = (123, 123, 'abc', 'abc')
+#tuple.append(123)
+#tuple.remove('abc')  # Give no option to add and remove the tuple list
+print(tuple)
+
+set = {123, 123, 'abc', 'abc'}
+set.add(678)
+set.add('def')
+set.remove('abc')
+print(set)
+
+dict = {'a' : 123, 'b' : 123, 'c' : 'abc', 'd' : 'def'}
+dict['e'] = 'def'
+dict['a'] = '456'
+dict.update({'b' : '4569'})
+del dict['b']
+print(dict)
