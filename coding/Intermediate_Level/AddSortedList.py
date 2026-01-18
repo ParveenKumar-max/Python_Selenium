6b2bca3343cdcb56ea3e1e3b5a61f1a55ac43482
parveendogra2@gmail.com <Parveendogra2@gmail.com>
2026-01-18 14:27:37 +0530

    Coding question of Python Intermediate_23

--- coding/Intermediate_Level/AddSortedList.py
+++ coding/Intermediate_Level/AddSortedList.py
@@ -5,7 +5,9 @@ list_two = [2,4,6,8,10]
 
 list_three = sorted(list_one + list_two)
 list_four = list(list_one + list_two)
+list_five = set(list_one + list_two)
 
 
-print("Sorted values are", list_three)
-print("Sorted values are", list_four)
+print("Sorted with added both list values are: ", list_three)
+print("Only merged both list values are: ", list_four)
+print(f"Merged both list but remove duplicate: {list_five}")
