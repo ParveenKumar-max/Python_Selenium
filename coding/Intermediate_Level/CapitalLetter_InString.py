3fc97a099da76246b179b5421f73c8633c74dfd2
parveendogra2@gmail.com <Parveendogra2@gmail.com>
2026-01-20 21:14:06 +0530

    Coding question of Python Intermediate_25

--- coding/Intermediate_Level/CapitalLetter_InString.py
+++ coding/Intermediate_Level/CapitalLetter_InString.py
@@ -1,9 +1,11 @@
-# cHECK HOW MANY Capital Letter in THE String
+# CHECK HOW MANY Capital Letter in THE String
 
 value = input("Enter Any Numbers: ")
-sums = 0
+sums = 0   # Create a integer variable
 for i in value:
     if i.isupper():
         sums = sums + 1
         print("The upper chars are :", i)
+    else:
+        print("The lower case value is :", i)
 print("Value in UpperCase:", value, "is", sums)
