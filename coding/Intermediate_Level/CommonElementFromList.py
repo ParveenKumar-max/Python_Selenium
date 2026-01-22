343a537b295c7c53202ddfa0a34674f054462e38
parveendogra2@gmail.com <Parveendogra2@gmail.com>
2026-01-22 23:24:41 +0530

    Coding question of Python Intermediate_25

--- coding/Intermediate_Level/CommonElementFromList.py
+++ coding/Intermediate_Level/CommonElementFromList.py
@@ -5,9 +5,14 @@ List1 = [1,2,3,4,4,5]
 List2 = [1,3,4,7,9,7]
 
 Common_Element = list(set(List1) & set(List2))
-print(Common_Element)
+print(f" Above line only prints Common Element", {Common_Element})
 # set(List1) & set(List2) is the set intersection operator "&",
-# which returns only the elements that are present in both sets; here that is {1, 3, 4}
+# which returns only the elements that are present in both sets; here that is [1, 3, 4]
 
 Remove_Duplicate = list(set(List1) | set(List2))
-print(Remove_Duplicate)
+print(f"Above line removes duplicat Element", {Remove_Duplicate})
+
+# set(List1) | set(List2) is the set union operator "|",
+# Returns a new set
+# Contains unique elements from both lists
+# Removes duplicates automatically here that is [1, 2, 3, 4, 5, 7, 9]
