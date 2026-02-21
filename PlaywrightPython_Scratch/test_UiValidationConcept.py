7b15c553d4a944fc2bb161ee3c46c4af67dc3017
parveendogra2@gmail.com <Parveendogra2@gmail.com>
2026-02-22 01:40:06 +0530

    Playwright Project from scratch 08

--- PlaywrightPython_Scratch/test_UiValidationConcept.py
+++ PlaywrightPython_Scratch/test_UiValidationConcept.py
@@ -2,7 +2,7 @@ import time
 
 from playwright.sync_api import Page, expect
 
-
+#hide/display and placeholder
 def test_CheckVisibleInvisibleChecks(page:Page):
     page.goto("https://rahulshettyacademy.com/AutomationPractice/")
     page.mouse.wheel(0,500)
@@ -12,6 +12,7 @@ def test_CheckVisibleInvisibleChecks(page:Page):
     expect(page.get_by_placeholder("Hide/Show Example")).to_be_hidden()
     time.sleep(5)
 
+#AlertBoxes
 def test_AlertPopsCheck(page:Page):
     page.goto("https://rahulshettyacademy.com/AutomationPractice/")
     page.get_by_role("button", name="Alert").click()
@@ -20,12 +21,52 @@ def test_AlertPopsCheck(page:Page):
     page.get_by_role("button", name="Confirm").click()
     time.sleep(5)
 
+#FrameHandling
 def test_iframeInOutChecks(page:Page):
     page.goto("https://rahulshettyacademy.com/AutomationPractice/")
     page.mouse.wheel(0,1000)
     Page_iframe_Store = page.frame_locator("#courses-iframe")
     Page_iframe_Store.get_by_role("link", name="All Access plan").click()
     Page_iframe_Store.locator("body").is_visible()
+
+#Check the price of rice is equal to 37.
+#identify the price column
+#identify rice row
+# extract the price of the rice.
+def test_HandleWebTables(page:Page):
+    page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers")
+    Table_Heading = page.locator("th").count()
+    print(Table_Heading)
+    PriceValueColumn = None
+    for index in range(page.locator("th").count()):
+        if page.locator("th").nth(index).filter(has_text="Price").count()>0:
+            PriceValueColumn = index
+            print("Price Column Value is : ", PriceValueColumn )
+            break
+    Rice_Row = page.locator("tr").filter(has_text="Rice")
+    expect(Rice_Row.locator("td").nth(PriceValueColumn)).to_have_text("37")
+
+#Same Web Tables login but different approach
+def test_HandleWebTables1(page: Page):
+    page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers")
+    headers = page.locator("th")
+    header_count = headers.count()
+
+    PriceValueColumn = None
+
+    for index in range(header_count):
+        if headers.nth(index).inner_text().strip() == "Price":
+            PriceValueColumn = index
+            print("Price Column Value is:", PriceValueColumn)
+            break
+
+    assert PriceValueColumn is not None, "Price column not found"
+
+    Rice_Row = page.locator("tbody tr").filter(has_text="Rice")
+    expect(Rice_Row.locator("td").nth(PriceValueColumn)).to_have_text("37")
+
+
+
 
 
 
