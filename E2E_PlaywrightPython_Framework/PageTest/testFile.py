5b2bc2008adbf996d486fe99bf936cc1244fb25f
parveendogra2@gmail.com <Parveendogra2@gmail.com>
2026-04-23 10:52:17 +0530

    Learning Basics again for Inerview

--- E2E_PlaywrightPython_Framework/PageTest/testFile.py
+++ E2E_PlaywrightPython_Framework/PageTest/testFile.py
@@ -2,3 +2,16 @@ A = [{"a" : "list1", "b" : "list2", "c" : 10}, {"a": "list1", "b" : "list2", "c"
 print(A)
 print(type(A))
 
+check = lambda x : x * 2
+print(check(2))
+
+check = lambda x : "Positive" if x > 0 else "Negative" if x < 0 else "ZERO"
+print(check(5))
+
+list_name = [1, 2, 3, 41, 5]
+Numbers = list(map(lambda x : x * 2, list_name))
+print(Numbers)
+
+list_name = [1, 2, 3, 41, 5]
+Numbers = list(filter(lambda x : x % 2 == 0, list_name))
+print(Numbers)
